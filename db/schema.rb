89fdc2907bfa1661bbc60eb555eb135bf8ba846f
Juan Pineiro <pineiro_juan@hotmail.com>
2015-09-13 02:18:29 -0700

    added images column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906131236) do
+ActiveRecord::Schema.define(version: 20150913082846) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150906131236) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "products", force: :cascade do |t|
